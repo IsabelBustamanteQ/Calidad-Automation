4cf38e90ccb11bbb572ea55dd4ff2389103e3f3b
unknown <isabqm121@gmail.com>
2024-11-27 20:33:04 -0400

    Añadir un escenario

--- features/step_definitions/gmo_home_pageSteps.rb
+++ features/step_definitions/gmo_home_pageSteps.rb
@@ -3,11 +3,7 @@
 When('I visit {string}') do |urlToSearch|
   visit(urlToSearch)
 end
-#   Then I see a text that says "Welcome to Green Mountain Outpost's OnLine Catalog!"
-Then('I see text that says {string}') do |welcomeMessage|
-  welcomeMessageLabel = find(:xpath, '/html/body/p[1]/font/em')
-  puts "ONLY FOR TEST PURPOSES:"+welcomeMessageLabel.text
-  if welcomeMessageLabel.text != welcomeMessage
-    	raise "Working hours should be"+welcomeMessage	
-  end
+# Then I see a text that says "Welcome to Green Mountain Outpost's OnLine Catalog!"
+Then(/^I see a text that says "([^"]*)"$/) do |welcomeMessage|
+  expect(page).to have_content(welcomeMessage)
 end

c5b35939bba6109cafe5b566a7f6cf35e160c616
Torrixo <santy.torrico@gmail.com>
2024-11-30 18:31:09 -0400

    refactor billing to use previous steps from navigate_to_billing

--- features/billing.feature
+++ features/billing.feature
@@ -4,10 +4,13 @@ Feature: Billing Information View
   So that I can place an order
 
   Background:
-    Given I am on the billing information page
+    Given I am on the main catalog page
+    When I add an item to the cart and place the order
+    And I proceed with the order
+    Then I should see the billing information page
 
-  Scenario: Filling out the billing form with valid data
-    When I fill in the billing form with valid details
+  Scenario: Billing and shipping information are the same
+  When I fill in the billing form with valid details
     | Field          | Value               |
     | Name           | John Doe            |
     | Address        | 123 Main St         |
@@ -18,6 +21,7 @@ Feature: Billing Information View
     | E-mail         | john@example.com    |
     | Card Type      | Visa                |
     | Card Number    | 4111111111111111    |
-    | Expiration     | 12/2026             |
-    And I submit the form
-    Then I should see a confirmation message "Order placed successfully"
+    | Expiration     | 12/94               |
+  And I check the "Same as Bill To" checkbox
+  And I submit the form
+  Then I should see a confirmation message "Order placed successfully"
--- features/step_definitions/billing.rb
+++ features/step_definitions/billing.rb
@@ -5,7 +5,43 @@ Given('I am on the billing information page') do
 end
 
 When('I fill in the billing form with valid details') do |table|
-  table.rows_hash.each do |field, value|
-    fill_in field, with: value
-  end
+  billing_data = table.rows_hash
+  fill_in 'billName', with: billing_data['Name']
+  fill_in 'billAddress', with: billing_data['Address']
+  fill_in 'billCity', with: billing_data['City']
+  fill_in 'billState', with: billing_data['State']
+  fill_in 'billZipCode', with: billing_data['Zip']
+  fill_in 'billPhone', with: billing_data['Phone']
+  fill_in 'billEmail', with: billing_data['E-mail']
+  select billing_data['Card Type'], from: 'CardType'
+  fill_in 'CardNumber', with: billing_data['Card Number']
+  fill_in 'CardDate', with: billing_data['Expiration']
+end
+
+When('I check the "Same as Bill To" checkbox') do
+    check 'shipSameAsBill'
+end
+
+When('I fill in the shipping form with valid details') do |table|
+    shipping_data = table.rows_hash
+
+    # Fill in shipping fields manually
+    fill_in 'shipName', with: shipping_data['Name']
+    fill_in 'shipAddress', with: shipping_data['Address']
+    fill_in 'shipCity', with: shipping_data['City']
+    fill_in 'shipState', with: shipping_data['State']
+    fill_in 'shipZipCode', with: shipping_data['Zip']
+    fill_in 'shipPhone', with: shipping_data['Phone']
+end
+
+When('I uncheck the "Same as Bill To" checkbox') do
+    uncheck 'shipSameAsBill'
+end
+
+And('I submit the form') do
+    click_button 'bSubmit'
+end
+
+Then('I should see a confirmation message {string}') do |message|
+    expect(page).to have_content(message)
 end

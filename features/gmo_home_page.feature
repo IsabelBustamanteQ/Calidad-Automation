f77232aa796f1d34faf5cfdb46dae3cee884df8d
unknown <isabqm121@gmail.com>
2024-12-01 19:41:37 -0400

    Actualizando escenarios

--- features/gmo_home_page.feature
+++ features/gmo_home_page.feature
@@ -4,6 +4,17 @@ Feature: As a internet user
 
 Scenario: Visit GMOPost and see welcome text
   Given I am on the Google homepage
-  When I visit "https://demo.borland.com/gmopost/"
+  When I visit GMO OnLine
   Then I see a text that says "Welcome to Green Mountain Outpost's OnLine Catalog!"
+  And I see a button that says "Enter GMO OnLine"
+  And I see a button that says "About The GMO Site"
+  And I see a button that says "Browser Test Page"
+  
+Scenario: Visit GMOPost catalog
+  Given I am on the Google homepage
+  When I visit GMO OnLine
+  And I will click "Enter GMO OnLine" button
+  Then I see a text that says "OnLine Catalog"
+  And I see a table with products to buy
+
   
--- features/step_definitions/gmo_home_pageSteps.rb
+++ features/step_definitions/gmo_home_pageSteps.rb
@@ -1,9 +1,25 @@
 
 #   When I visit https://demo.borland.com/gmopost/
-When('I visit {string}') do |urlToSearch|
-  visit(urlToSearch)
+When('I visit GMO OnLine') do
+  visit('https://demo.borland.com/gmopost/')
 end
 # Then I see a text that says "Welcome to Green Mountain Outpost's OnLine Catalog!"
-Then(/^I see a text that says "([^"]*)"$/) do |welcomeMessage|
-  expect(page).to have_content(welcomeMessage)
+Then(/^I see a text that says "([^"]*)"$/) do |contentMessage|
+  expect(page).to have_content(contentMessage)
+end
+  # And I see a button that says "Enter GMO OnLine"
+Then (/^I see a button that says "([^"]*)"$/) do |buttonMessage|
+  expect(page).to have_button(buttonMessage)
+end
+# And I will click "Enter GMO OnLine" button
+When(/^I will click "([^"]*)" button$/) do |enterButton|
+  click_button(enterButton)
+end
+# And I see a table with products to buy
+Then('I see a table with products to buy') do
+  address = find(:xpath, '/html/body/form/table/tbody/tr[2]/td/div/center/table/tbody/tr[2]/td[2]/a')
+  
+  if address.text != "3 Person Dome Tent"
+    raise "Product not found"
+  end
 end

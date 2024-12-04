46044395d9e7c1f7a7145b1353ed6b6658d16b0e
santytorrico <santy.torrico@gmail.com>
2024-12-04 19:26:12 -0400

    Revert "Enhance order confirmation page test verification"
    This reverts commit 28aa897785ee300206d75bc19b4dad10998cab6a.

--- features/step_definitions/billing.rb
+++ features/step_definitions/billing.rb
@@ -116,16 +116,7 @@ end
 Then('I should see the order confirmation page with different bill,ship data') do
     expect(page).to have_content('Thank you for shopping with Green Mountain Outpost')
 end
-# Then('I should see the order confirmation page') do
-#     expect(page).to have_content('Thank you for shopping with Green Mountain Outpost')
-#     expect(page).to have_content('John Doe')
-# end
-Then('I should see the order confirmation page') do |table|
-    billing_data = table.rows_hash
-    
-    # Verify the confirmation message
+Then('I should see the order confirmation page') do
     expect(page).to have_content('Thank you for shopping with Green Mountain Outpost')
-    
-    # Dynamically verify the name from the input data
-    expect(page).to have_content(billing_data['Name'])
-  end
+    expect(page).to have_content('John Doe')
+end

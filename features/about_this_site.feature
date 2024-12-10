4d86a4ad1e5bbb44ce4b2e959c98bbc10178fbee
unknown <isabqm121@gmail.com>
2024-12-10 18:40:57 -0400

    updating about this site step

--- features/about_this_site.feature
+++ features/about_this_site.feature
@@ -6,7 +6,7 @@ Scenario: Visit GMOPost About This Site Page
   Given I am on the Google homepage
   When I visit GMO OnLine
   And I will click "About The GMO Site" button
-  Then I see a text that says "About This Site"
+  Then I see the About The GMO Site
   And I see a warning that says "DO NOT ENTER REAL BILLING OR SHIPPING INFORMATION"
   And I see a table with the technologies used by this site
   
--- features/step_definitions/about_this_site.rb
+++ features/step_definitions/about_this_site.rb
@@ -1,8 +1,13 @@
+# Then I see the About The GMO Site
+Then ("I see the About The GMO Site") do 
+  expect(@about_page.on_about_this_site_page?).to be true
+end
+# And I see a warning that says "DO NOT ENTER REAL BILLING OR SHIPPING INFORMATION"
 Then (/^I see a warning that says "([^"]*)"$/) do |warning_message|
   expect(@about_page.has_warning_message?(warning_message)).to be true
 end
 
-
+# And I see a table with the technologies used by this site
 Then('I see a table with the technologies used by this site') do
   expect(@about_page.rows_have_information?).to be true
 end

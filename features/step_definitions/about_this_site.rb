2a090a1e8fad5b255e1aea8df2f6ab1c924ccaf8
unknown <isabqm121@gmail.com>
2024-12-10 16:30:22 -0400

    Updating at PMO

--- features/step_definitions/about_this_site.rb
+++ features/step_definitions/about_this_site.rb
@@ -1,15 +1,10 @@
-#   And I see a warning that says "DO NOT ENTER REAL BILLING OR SHIPPING INFORMATION"
-Then (/^I see a warning that says "([^"]*)"$/) do |warningMessage|
-    expect(page).to have_content(warningMessage)
-  end
-#   And I see a table with the technologies used by this site
+Then (/^I see a warning that says "([^"]*)"$/) do |warning_message|
+  @about_page = AboutThisSitePage.new
+  expect(@about_page.has_warning_message?(warning_message)).to be true
+end
+
+
 Then('I see a table with the technologies used by this site') do
-    technologies_table = find(:xpath, "/html/body/div/center/table")
-    technologies_rows = technologies_table.all(:xpath, "tbody/tr")
-    expect(technologies_rows.size).to be > 1
-    technologies_rows.drop(1).each do |row|
-      technology_name = row.find(:xpath, "td[1]/blockquote/ul/li[1]/font").text
-      expect(technology_name).not_to be_empty, "Found an empty technology name in the table."
-    end
-  end
-  
+  @about_page ||= AboutThisSitePage.new
+  expect(@about_page.rows_have_information?).to be true
+end

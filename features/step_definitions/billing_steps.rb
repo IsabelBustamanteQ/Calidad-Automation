6c997d7072d6f20959d09891a313745c03bcfe4e
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-12-11 18:57:13 -0400

    Se agrego el reporte final

--- features/step_definitions/billing_steps.rb
+++ features/step_definitions/billing_steps.rb
@@ -1,3 +1,11 @@
+When('I click the Proceed With Order button') do
+  @place_order_page.click_order_button
+end
+
+Then('I should see the billing information page') do
+  expect(@billing_page.on_billing_page?).to be true
+end
+
 When('I fill in the billing form with valid details') do |table|
   @billing_page.fill_billing_form(table.rows_hash)
 end
